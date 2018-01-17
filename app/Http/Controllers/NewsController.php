813e628f79d252e4dbbe3685b4891a410d5d2f90
Candy <bulochka1329@gmail.com>
2018-01-17 02:08:27 +0200

    'addchange'

--- app/Http/Controllers/NewsController.php
+++ app/Http/Controllers/NewsController.php
@@ -214,12 +214,12 @@ class NewsController extends Controller
     public function newsCardImage($image)
     {
         try {
-            return Image::make('storage/app/public' . '/' . $image)->response();
+            return Image::make('/storage/app/public' . '/' . $image)->response();
         } catch(\Exception $e) {
             echo "<pre>";
             echo $e;
             echo "</pre>";
         }
-        return Image::make('storage/app/public' . '/' . $image)->response();
+        return Image::make('/storage/app/public' . '/' . $image)->response();
     }
 }
