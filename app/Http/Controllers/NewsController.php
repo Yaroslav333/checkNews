99cd65d8dbb39d40a77587c67e8cb2ba68f587e2
Candy <bulochka1329@gmail.com>
2018-01-17 03:12:54 +0200

    'addchange'

--- app/Http/Controllers/NewsController.php
+++ app/Http/Controllers/NewsController.php
@@ -227,7 +227,7 @@ class NewsController extends Controller
 
 
         try {
-            dd(Storage::get(Storage::url('zFi0mS4NT3klbCx7VQ9oLyxTsY0AWoaHkOs2NZWS.jpeg')));
+            dd(Storage::get('/app/storage' . '/' . $image));
             //dd( Storage::url('zFi0mS4NT3klbCx7VQ9oLyxTsY0AWoaHkOs2NZWS.jpeg'));
             //dd(scandir('/app/storage/app/public'));
             //return Image::make(public_path('storage/' . $image))->response();
